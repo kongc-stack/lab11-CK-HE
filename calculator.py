08d3fad9329f4d5975712575016fa58efa24d047
Catherine Kong <catherinekong103@gmail.com>
2025-11-15 15:34:35 -0500

    calculator functions- new two functions

--- calculator.py
+++ calculator.py
@@ -4,8 +4,39 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
+
+
+
+import math
+
+#try and catch errors for two new functions?
+# what does this mean?
+
+
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    else:
+        return b / a
+
+def logarithm(a, b):
+    if a != "e" or a < 1 or b < 1:
+        raise ValueError
+    else:
+        return math.log(b, a)# use math library + raise ValueError
+
+def exponent(a, b):
+    return a ** b
+
 
 
